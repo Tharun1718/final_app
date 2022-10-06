27fc9b9536980dd606d755971ac1de7c6038871c
Tharun1718 <tharunmano1170@gmail.com>
2022-10-06 19:28:46 +0530

    connected to heroku

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect} from "react";
+
+// const API="http://localhost:4000"
+
+const API = "https://tharun-node.herokuapp.com"
 
 function App() {
+  const [mobiles, setMobiles] = useState([])
+
+  useEffect(() =>{
+    fetch(`${API}/mobiles`)
+    .then((res)=> res.json())
+    .then((mbs)=> setMobiles(mbs))
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="phone-list-container">
+      {mobiles.map((mob)=>(
+        <Phone key={mob._id} mobile={mob} />
+      ))}
+      </div>
     </div>
   );
+}
+
+
+function Phone({mobile}){
+  
+  return(
+    <div className="phone-container">
+      <img className="phone-picture" src={mobile.img} alt={mobile.model} />
+      <h2 className="phone-name">{mobile.model}</h2>
+      <p className="phone-company">{mobile.company}</p>
+    </div>
+  )
 }
 
 export default App;
